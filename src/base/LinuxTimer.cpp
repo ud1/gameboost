f2bb52bee0f8bb98795509f29514969e8e93a002
Enegine@gmail.com <Enegine@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-08 20:32:17 +0000

    Добавлен класс таймера

---
+++ src/base/LinuxTimer.cpp
@@ -0,0 +1,80 @@
+#ifndef _WIN32
+
+#include "Timer.h"
+#include <assert.h>
+#include <sys/time.h>
+
+//
+// Timer
+//
+
+uint64_t Time()
+{
+	timeval t;
+	gettimeofday(&t, nullptr);
+	return (uint64_t) t.tv_sec*1000000 + t.tv_usec;
+}
+
+Timer::Timer()
+{
+	start_time = Time();
+	global_start_time = start_time;
+}
+
+// Засекаем текущее время
+void Timer::startTiming()
+{
+	start_time = Time();
+}
+
+// Возвращает прошедшее время
+double Timer::timeElapsed()
+{
+	elapsed_time = ((double)( Time() - start_time )/1000000.0);
+	return elapsed_time;
+}
+
+double Timer::globalTime()
+{
+	uint64_t temp = global_start_time;
+	global_start_time = Time();
+	global_time += ((double)( global_start_time - temp )/1000000.0);
+	return global_time;
+}
+
+void Timer::resetGlobalTime(double time)
+{
+	global_start_time = Time();
+	global_time = time;
+}
+
+//
+// AdvancedTimer
+//
+
+AdvancedTimer::AdvancedTimer()
+{
+	time_acceleration = 1.0;
+}
+
+double AdvancedTimer::timeElapsed()
+{
+	elapsed_time=((double)( Time() - start_time)/1000000.0 )*time_acceleration;
+	return elapsed_time;
+}
+
+double AdvancedTimer::globalTime()
+{
+	uint64_t temp = global_start_time;
+	global_start_time = Time();
+	global_time += ((double)( global_start_time - temp )/1000000.0)*time_acceleration;
+	return global_time;
+}
+
+void AdvancedTimer::setTimeAcceleration(double accel)
+{
+	globalTime();
+	time_acceleration = accel;
+}
+
+#endif
