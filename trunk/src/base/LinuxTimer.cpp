21a16d566fd06ac0be1a4180a899d2c5f2e47de6
Enegine@gmail.com <Enegine@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-08 20:37:47 +0000

    Правка таймера

--- trunk/src/base/LinuxTimer.cpp
+++ trunk/src/base/LinuxTimer.cpp
@@ -4,77 +4,88 @@
 #include <assert.h>
 #include <sys/time.h>
 
-//
-// Timer
-//
-
-uint64_t Time()
+namespace
 {
-	timeval t;
-	gettimeofday(&t, nullptr);
-	return (uint64_t) t.tv_sec*1000000 + t.tv_usec;
+	uint64_t Time()
+	{
+		timeval t;
+		gettimeofday(&t, nullptr);
+		return (uint64_t) t.tv_sec*1000000 + t.tv_usec;
+	}
 }
 
-Timer::Timer()
+namespace gb
 {
-	start_time = Time();
-	global_start_time = start_time;
-}
+	namespace base
+	{
+		
+		//
+		// Timer
+		//
 
-// Засекаем текущее время
-void Timer::startTiming()
-{
-	start_time = Time();
-}
+		Timer::Timer()
+		{
+			start_time = Time();
+			global_start_time = start_time;
+		}
 
-// Возвращает прошедшее время
-double Timer::timeElapsed()
-{
-	elapsed_time = ((double)( Time() - start_time )/1000000.0);
-	return elapsed_time;
-}
+		// Засекаем текущее время
+		void Timer::startTiming()
+		{
+			start_time = Time();
+		}
 
-double Timer::globalTime()
-{
-	uint64_t temp = global_start_time;
-	global_start_time = Time();
-	global_time += ((double)( global_start_time - temp )/1000000.0);
-	return global_time;
-}
+		// Возвращает прошедшее время
+		double Timer::timeElapsed()
+		{
+			elapsed_time = ((double)( Time() - start_time )/1000000.0);
+			return elapsed_time;
+		}
 
-void Timer::resetGlobalTime(double time)
-{
-	global_start_time = Time();
-	global_time = time;
-}
+		double Timer::globalTime()
+		{
+			uint64_t temp = global_start_time;
+			global_start_time = Time();
+			global_time += ((double)( global_start_time - temp )/1000000.0);
+			return global_time;
+		}
 
-//
-// AdvancedTimer
-//
+		void Timer::resetGlobalTime(double time)
+		{
+			global_start_time = Time();
+			global_time = time;
+		}
 
-AdvancedTimer::AdvancedTimer()
-{
-	time_acceleration = 1.0;
-}
+		//
+		// AdvancedTimer
+		//
 
-double AdvancedTimer::timeElapsed()
-{
-	elapsed_time=((double)( Time() - start_time)/1000000.0 )*time_acceleration;
-	return elapsed_time;
-}
+		AdvancedTimer::AdvancedTimer()
+		{
+			time_acceleration = 1.0;
+		}
 
-double AdvancedTimer::globalTime()
-{
-	uint64_t temp = global_start_time;
-	global_start_time = Time();
-	global_time += ((double)( global_start_time - temp )/1000000.0)*time_acceleration;
-	return global_time;
-}
+		double AdvancedTimer::timeElapsed()
+		{
+			elapsed_time=((double)( Time() - start_time)/1000000.0 )*time_acceleration;
+			return elapsed_time;
+		}
 
-void AdvancedTimer::setTimeAcceleration(double accel)
-{
-	globalTime();
-	time_acceleration = accel;
+		double AdvancedTimer::globalTime()
+		{
+			uint64_t temp = global_start_time;
+			global_start_time = Time();
+			global_time += ((double)( global_start_time - temp )/1000000.0)*time_acceleration;
+			return global_time;
+		}
+
+		void AdvancedTimer::setTimeAcceleration(double accel)
+		{
+			globalTime();
+			time_acceleration = accel;
+		}
+
+	}
 }
 
 #endif
