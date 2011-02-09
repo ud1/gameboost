ffc54e240e6d6c21dfb6cedddf0e448655de0d10
Enegine@gmail.com <Enegine@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-09 18:55:25 +0000

    Изменения в таймере

--- src/base/Timer.cpp
+++ src/base/Timer.cpp
@@ -1,77 +1,122 @@
 ﻿#include "base/Timer.h"
 
+namespace
+{
+
+#ifdef _WIN32
+	#include <Windows.h>
+	
+	// Частота
+	uint64_t freq;
+	
+	inline uint64_t Time()
+	{
+		LARGE_INTEGER s;
+		QueryPerformanceCounter(&s);
+		return s.QuadPart;
+	}
+	
+	void setupFreq()
+	{
+		LARGE_INTEGER s;
+		QueryPerformanceFrequency(&s);
+		freq = s.QuadPart;
+	}
+
+#else
+	#include <sys/time.h>
+	#include <unistd.h>
+	
+	inline uint64_t Time()
+	{
+		timeval t;
+		gettimeofday(&t, 0);
+		return (uint64_t) t.tv_sec*1000000 + t.tv_usec;
+	}
+	
+#define setupFreq() 0
+#define freq 1000000
+
+#endif
+
+}
+
 namespace gb
 {
 	namespace base
 	{
+		
+		//
+		// Timer
+		//
 
-		//----------------------------------------------------------------------
-
-		FPSTimer::FPSTimer()
+		Timer::Timer()
 		{
-			time = 0.0;
-			dt = 0.0f;
-			adt = 0.0f;
-			fps = 0.0f;
-			afps = 0.0f;
-			frames = 0;
+			setupFreq();
+			start_time = getTickCount();
 		}
-
-		//----------------------------------------------------------------------
-
-		float FPSTimer::finishTiming()
+		
+		void Timer::sleep(unsigned millisecs)
 		{
-
-			double elaps = timeElapsed();
-			dt = float(elaps);
-
-			if (elaps > 1e-10) {
-				fps	= float(1.0 / elaps);
-			} else {
-				fps = 0.0f;
-			}
-
-			time += elaps;
-			frames ++;
-
-			if (time > 1.0) {
-				afps = float(double(frames) / time);
-				time = 0.0;
-				frames = 0;
-			}
-			adt = adt * 0.5f + dt * 0.5f;
-			return fps;
-		}
-
-		//----------------------------------------------------------------------
-		
-		float getDt() const {
-			return dt;
+#ifdef _WIN32
+			Sleep(millisecs);
+#else
+			usleep(millisecs*1000);
+#endif
 		}
 		
-		//----------------------------------------------------------------------
-		
-		float getAverageDt() const
+		Timer::Ticks Timer::getTickCount() const
 		{
-			return adt;
+			return (Ticks) Time();
 		}
 		
-		//----------------------------------------------------------------------
-		
-		float getFPS() const
+		Timer::TicksFreq Timer::getTimeFreq() const
 		{
-			return fps;
+			return freq;
 		}
-		
-		//----------------------------------------------------------------------
-		
-		float getAverageFPS() const 
+
+		// Засекаем текущее время
+		void Timer::startTiming()
 		{
-			return afps
+			start_time = getTickCount();
+		}
+
+		// Возвращает прошедшее время
+		double Timer::timeElapsed()
+		{
+			elapsed_time = ((double)( getTickCount() - start_time )/getTimeFreq());
+			return elapsed_time;
+		}
+
+		//
+		// AdvancedTimer
+		//
+
+		AdvancedTimer::AdvancedTimer()
+		{
+			time_acceleration = 1.0;
+			the_time = 0.0;
+		}
+
+		void AdvancedTimer::resetTime(double t_)
+		{
+			the_time = t;
+			timer.startTiming();
 		}
 		
-		//----------------------------------------------------------------------
-		
+		double AdvancedTimer::getTime()
+		{
+			t = the_time + timer.timeElapsed()*time_acceleration;
+			return t;
+		}
+
+		void AdvancedTimer::setTimeAccel(double accel)
+		{
+			the_time = getTime();
+			timer.startTiming();
+			time_acceleration = accel;
+		}
+
 	}
-
 }
+
