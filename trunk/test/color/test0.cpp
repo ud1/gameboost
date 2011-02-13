04b57ea7c9564a69635d803b9008808361adca6a
ksacvet777@gmail.com <ksacvet777@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-13 17:48:34 +0000

    load color test cpp

---
+++ trunk/test/color/test0.cpp
@@ -0,0 +1,17 @@
+//* test color
+
+#include "stdafx.h"
+#include "Z:\\!!GB\\gameboost\\gb\\color\\color.h"
+
+
+
+//=======================================================================
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+
+
+
+	return 0;
+}
+
