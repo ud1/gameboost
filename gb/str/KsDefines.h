9a413c3d3479bb7bc30796ceba822f496d733bce
ksacvet777@gmail.com <ksacvet777@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-14 19:38:14 +0000

    small some changes

--- gb/str/KsDefines.h
+++ gb/str/KsDefines.h
@@ -5,12 +5,22 @@
  *
  *
  */
+
 #pragma once
 
-#define KS_IN /* in */
-#define KS_OUT /* out */
-#define KS_INOUT /* in-out */
-#define KS_IN_OPTIONAL /* in optional */
-#define KS_OUT_OPTIONAL /* out optional */
+/* in */
+#define KS_IN   const
+
+/* out */
+#define KS_OUT   
+
+/* in-out */
+#define KS_INOUT   
+
+/* in optional */
+#define KS_IN_OPTIONAL	 const
+
+/* out optional */
+#define KS_OUT_OPTIONAL    
 
 
