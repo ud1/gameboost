b0a7522d554aa51e35deb3a000f5bc34a64e1ab6
Pushkoff@gmail.com <Pushkoff@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-14 00:55:57 +0000

    [pmath] separated declaration and realization of vector and matrix [pmath] added short typenames (mat4,vec3,etc)

--- defines.h
+++ defines.h
@@ -2,11 +2,11 @@
 #define MATH_DEFINES_H
 
 #ifndef PMATH_ASSERT
-#define PMATH_ASSERT(expression)
+#define PMATH_ASSERT(expression) assert(!!(expression))
 #endif
 
 #ifndef PMATH_STATIC_ASSERT
-#define PMATH_STATIC_ASSERT(expression, message)
+#define PMATH_STATIC_ASSERT(expression, message) static_assert( !!(expression), message )
 #endif
 
 #endif // MATH_DEFINES_H
--- pmath.h
+++ pmath.h
@@ -1,6 +1,7 @@
 #ifndef PMATH_HEADER
 #define PMATH_HEADER
 
+#include <assert.h>
 #include <math.h>
 #include <algorithm>
 #include <float.h>
@@ -8,6 +9,9 @@
 
 #include <cmath>
 #include <utility>
+
+namespace gb
+{
 
 #include "defines.h"
 #include "Algorytm.h"
@@ -17,5 +21,19 @@
 #include "Rect.h"
 #include "AABBox.h"
 #include "Ray.h"
+
+}
+
+namespace gb
+{
+	namespace pmath
+	{
+
+typedef Matrix<float,4> mat4;
+typedef Vector<float,4> vec4;
+typedef Vector<float,3> vec3;
+typedef Vector<float,2> vec2;
+	}
+}
 
 #endif // PMATH_HEADER
