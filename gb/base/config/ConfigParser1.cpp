8ad3c31364ce1a534bfd174fa267eb34d40456b5
zefick@mail.ru <zefick@mail.ru@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-15 16:07:23 +0000

    classes VariableMap & ConfigParser1

---
+++ gb/base/config/ConfigParser1.cpp
@@ -0,0 +1,132 @@
+﻿
+/**
+ * @file ConfigParser1.cpp
+ * @brief Реализация функций класса ConfigParser1 - парсера файла с параметрами
+ * @author Zefick 2010-2011
+ */
+
+#include "pch.h"
+
+#include <cstdio>
+#include <cstring>
+
+#include "ConfigParser1.h"
+
+namespace gb {
+namespace base {
+namespace config {
+
+
+ConfigParser1::ConfigParser1 () {}
+
+ConfigParser1::~ConfigParser1 () {
+    if (heap)
+        delete[] heap;
+}
+
+//! Лексический разбор текста
+void ConfigParser1::tokenize (const char *str) {
+
+    heap = new char[strlen(str)*2];
+    char * top = heap;
+
+    for (; *str!='\0'; ++str) {
+        if (*str == '=') {
+            mas.push_back(token(assign, top));
+            *top++ = '=';
+            *top++ = '\0';
+        }
+        else if (*str == '{') {
+            mas.push_back(token(lbrace, top));
+            *top++ = '{';
+            *top++ = '\0';
+        }
+        else if (*str == '}') {
+            mas.push_back(token(rbrace, top));
+            *top++ = '}';
+            *top++ = '\0';
+        }
+        // skip comments
+        else if (str[0] == '/' && str[1]=='/') {
+            while(*str != '\n') str++;
+        }
+        // quoted string
+        else if (*str == '"') {
+            mas.push_back(token(word, top));
+            str++;
+            while(*str != '"') *top++ = *str++;
+            *top++ = '\0';
+        }
+        // unquoted value
+        else if (isalnum(*str) || *str=='_') {
+            mas.push_back(token(word, top));
+            while(isalnum(*str) || *str=='_') *top++ = *str++;
+            *top++ = '\0';
+            str--;
+        }
+    }
+    mas.push_back(token(end, NULL));
+}
+
+std::vector<ConfigParser1::token>::iterator it;
+
+//! Начало разбора
+void ConfigParser1::parse (const char *text, VariableMap *_conf) {
+    config = _conf;
+    tokenize(text);
+    it = mas.begin();
+    U("");
+}
+
+//! Печать ошибки о неправильной лексеме
+void ConfigParser1::illegal (const char *t) {
+    char message[200];
+    sprintf(message, "illegal token: \"%s\" !", t);
+    throw parseException(message);
+}
+
+
+/*
+  U :: NSU | e
+  S :: {U} | = V | V
+  N :: <name>
+  V :: <value>
+*/
+
+void ConfigParser1::U (const std::string section) {
+
+    while (it->first == word) {
+        std::string newSection = section + it->second;
+        ++it;
+        //puts(newSection.c_str());
+        S (newSection);
+    }
+    return;
+}
+
+
+void ConfigParser1::S (const std::string section) {
+
+    if (it->first == lbrace) {
+        ++it;
+        U (section + ".");
+        if (it->first == rbrace) {
+            ++it;
+            return;
+        }
+        else illegal(it->second);
+    }
+    else if (it->first == assign) {
+        ++it;
+        if (it->first != word)
+            illegal(it->second);
+        config->setParamStr(section, it->second);  /**/
+        ++it;
+        return;
+    }
+    else
+        illegal(it->second);
+}
+
+// namespaces
+}}}
---
+++ gb/base/config/VariableMap.h
@@ -0,0 +1,136 @@
+﻿
+/**
+ * @file VariableMap.h
+ * @brief Заголовок класса VariableMap - контейнера параметров
+ * @author Шаров Сергей, Zefick 2010-2011
+ */
+
+#ifndef VARIABLE_MAP
+#define VARIABLE_MAP
+
+#include <string>
+#include <sstream>
+#include <map>
+
+namespace gb {
+namespace base {
+namespace config {
+
+/**
+ * Исключение, которое должны выбрасывать парсеры при ошибке разбора
+ */
+struct parseException {
+    std::string message;
+    parseException (const std::string & msg) : message(msg) {}
+    const char * what () {return message.c_str();}
+};
+
+/**
+ * Интерфейс парсера конфигурационного файла
+ */
+class IParser {
+protected:
+    friend class VariableMap;
+    void parse(const char *text, class VariableMap *conf);
+};
+
+/**
+ * Класс для хранения именованных значений
+ */
+class VariableMap {
+
+    public:
+
+        VariableMap();
+
+        ~VariableMap();
+
+        /**
+         * Получение строки, хранящей значение параметра
+         * @param name имя параметра
+         * @return значение параметра в виде строки
+         */
+        std::string getParamStr (std::string name) const;
+
+        /**
+         * Задание значения параметра
+         * @param name имя параметра
+         * @param value задаваемое значение параметра
+         */
+        void setParamStr (const std::string & name, const std::string & value);
+
+        /**
+         * Чтение набора параметров из файла с помощью специализированного парсера.
+         * @param Parser класс парсера
+         * @param filename путь к файлу
+         */
+        template <class Parser>
+        void parseFile (const char * filename) {
+            try {
+                Parser parser;
+                const char *text = openFile(filename);
+                parser.parse(text, this);
+                delete text;
+            }
+            catch (parseException ex) {
+                puts(ex.what());
+            }
+        }
+
+        /**
+         * Чтение набора параметров из строки с помощью специализированного парсера.
+         * @param Parser класс парсера
+         * @param filename путь к файлу
+         */
+        template <class Parser>
+        void parse (const char * text) {
+            try {
+                Parser parser;
+                parser.parse(text, this);
+            }
+            catch (parseException ex) {
+                puts(ex.what());
+            }
+        }
+
+        /**
+         * Получение значения параметра заданного типа.
+         * @param T нужный тип
+         * @param name имя параметра
+         * @param def_value значение по умолчанию для незаданного параметра
+         */
+        template <typename T>
+        T get (const std::string name, T def_value = T()) const {
+            T res = def_value;
+            std::string str = getParamStr(name);
+            if (!str.empty()) {
+                std::istringstream S(str);
+                S >> res;
+            }
+            return res;
+        }
+
+        /**
+         * Задание значения параметра заданного типа
+         * @param T нужный тип
+         * @param name имя параметра
+         * @param value задаваемое значение параметра
+         */
+        template <typename T>
+        void set (const char* name, const T& value) {
+            std::ostringstream S;
+            S << value;
+            setParamStr(name, S.str());
+        }
+
+    private:
+
+        std::map<std::string, std::string> vmap;
+
+        char * openFile (const char *filename);
+};
+
+// namespaces
+}}}
+
+#endif // VARIABLE_MAP
