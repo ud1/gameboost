bd0bbcd68048cc9dce0ba59561b652fed0a330e7
madness.cpp@gmail.com <madness.cpp@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-18 09:44:56 +0000

    KsMath: закомментировал константы, пользуемся gb/base/constants.h

--- gb/math/scalar.h
+++ gb/math/scalar.h
@@ -13,6 +13,7 @@
 #include <float.h>
 #include <stdexcept>
 
+#include <gb/base/Constants.h>
 
 namespace gb 
 {
@@ -25,30 +26,32 @@ namespace gb
 	{
 	
 
-	static const float CPI       = 3.1415926536f; ///<   PI
-	static const float C1BYPI    = 0.3183098862f; ///<   1/PI	
+		// Константы добавлять и брать в gb/base/Constants.h
+
+// 	static const float CPI       = 3.1415926536f; ///<   PI
+// 	static const float C1BYPI    = 0.3183098862f; ///<   1/PI	
 	
-	static const float C2PI         =  6.283185307f; ///<  	2pi
-	static const float CPIDIV2      =  1.570796326f; ///<   pi/2
-	static const float CPIDIV4      =  0.785398163f; ///<   pi/4
-	static const float CPIDIV6      =  0.523598776f; ///<   pi/6
-
-	static const float CPIDIV180    =  0.017453292f; ///<   pi/180
-	static const float C180DIVPI    =  57.29577951f; ///<   180/pi
-
-	static const float C3PIDIV4     = 2.35619449f;   ///<   3/4 pi
-	static const float CINV2PI      = 0.159154943f;  ///<   1/(2pi)
-	static const float CINV360      = 0.002777778f;  ///<   1/360
-	static const float C180         = 180.0f;
-	static const float C360         = 360.0f;
-	static const float CONEHALF     = 0.5f;
-	static const float CTWO         = 2.0f;
-	static const float CLN10        = 2.302585093f;
-
-	static const float CSQRT2       = 1.4142135624f; ///< sqrt(2)
-	static const float CSQRTHALF    = 0.7071067812f; ///< sqrt(1/2)
-	
-	static const float CE           = 2.718281828459f; ///<  e
+// 	static const float C2PI         =  6.283185307f; ///<  	2pi
+// 	static const float CPIDIV2      =  1.570796326f; ///<   pi/2
+// 	static const float CPIDIV4      =  0.785398163f; ///<   pi/4
+// 	static const float CPIDIV6      =  0.523598776f; ///<   pi/6
+// 
+// 	static const float CPIDIV180    =  0.017453292f; ///<   pi/180
+// 	static const float C180DIVPI    =  57.29577951f; ///<   180/pi
+// 
+// 	static const float C3PIDIV4     = 2.35619449f;   ///<   3/4 pi
+// 	static const float CINV2PI      = 0.159154943f;  ///<   1/(2pi)
+// 	static const float CINV360      = 0.002777778f;  ///<   1/360
+// 	static const float C180         = 180.0f;
+// 	static const float C360         = 360.0f;
+// 	static const float CONEHALF     = 0.5f;
+// 	static const float CTWO         = 2.0f;
+// 	static const float CLN10        = 2.302585093f;
+// 
+// 	static const float CSQRT2       = 1.4142135624f; ///< sqrt(2)
+// 	static const float CSQRTHALF    = 0.7071067812f; ///< sqrt(1/2)
+// 	
+// 	static const float CE           = 2.718281828459f; ///<  e
 	
 	
 	/**  \brief Возвращает единицу с соответствующим знаком или ноль если параметр ноль   */
@@ -82,9 +85,9 @@ namespace gb
     inline float cotan(const float f) {  return   1.0f / tanf(f); };
 	
 	/** \brief Угол в радианах в градусы */
-	inline float radiansToDegree(float rad)  { return rad * (180.0f / CPI); };
+	inline float radiansToDegree(float rad)  { return rad * ( 180.0f / gb::constPi <float>() ); };
 	/** \brief Угол в градусах в радианы */	
-	inline float degreeToRadians(float dgr)  { return dgr * (CPI / 180.0f); };
+	inline float degreeToRadians(float dgr)  { return dgr * (gb::constPi <float>() / 180.0f); };
 	
 	/**  \brief  Вычисленить линейную интерполяцию между f1 и f2 по коэф. k */
     inline float lerp(const float f1, const float f2, const float k) { return f1 + (f2 - f1) * k; };
