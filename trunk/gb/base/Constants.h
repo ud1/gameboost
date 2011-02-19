ce764205a125249d1134830e8310e2705cf8820a
madness.cpp@gmail.com <madness.cpp@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-19 16:41:36 +0000

    буст теперь ищется в gb/deplib/boost, или в c:/boost (делайте символьные ссылки на существующий инсталл буста так даже удобнее) добавлен C++ модуль для констант, добавлены инлайны в функции чтобы побороть ошибку (function already defined)

--- trunk/gb/base/Constants.h
+++ trunk/gb/base/Constants.h
@@ -4,37 +4,39 @@
  *	Фундаментальные и вспомогательные константы
  *	\author Дмитрий Литовченко kvakvs@yandex.ru
  */
+#include <limits>
 
 namespace gb
 {
 
-	double getNaN() {
-		unsigned long nan_ulong[2] = {0xffffffff, 0x7fffffff};
-		return *( double* )nan_ulong;
+	template <typename value_type>
+	inline value_type getNaN()
+	{
+		return std::numeric_limits <value_type>::quiet_NaN();
 	}
 
-#define GB_DECLARE_FLOAT_DOUBLE_CONST( NAME, VALUE ) \
-	namespace detail { \
-		static const double d_const##NAME = VALUE; \
-		static const float f_const##NAME = (float)VALUE; \
-	} \
-	template <typename value_type> value_type const##NAME(); \
-	template<> float const##NAME() { return gb::detail::f_const##NAME; } \
-	template<> double const##NAME() { return gb::detail::d_const##NAME; }
+#define GB_DECLARE_FLOAT_DOUBLE_CONST( NAME ) \
+			namespace detail { \
+				extern const double d_const##NAME; \
+				extern const float f_const##NAME; \
+			} \
+			template <typename value_type> value_type const##NAME(); \
+			template<> inline float const##NAME() { return gb::detail::f_const##NAME; } \
+			template<> inline double const##NAME() { return gb::detail::d_const##NAME; }
 
-	GB_DECLARE_FLOAT_DOUBLE_CONST( E, 2.71828182845904523536 );
-	GB_DECLARE_FLOAT_DOUBLE_CONST( Pi, 3.14159265358979323846 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( 2_Pi, 6.28318530717958647692 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( Pi_Div_2, 1.57079632679489661923 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( Pi_Div_4, 0.785398163397448309616 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( 1_Div_Pi, 0.318309886183790671538 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( 2_Div_Pi, 0.636619772367581343076 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( Sqrt_2, 1.41421356237309504880 );
+	GB_DECLARE_FLOAT_DOUBLE_CONST( E );
+	GB_DECLARE_FLOAT_DOUBLE_CONST( Pi );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( 2_Pi );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( Pi_Div_2 );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( Pi_Div_4 );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( 1_Div_Pi );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( 2_Div_Pi );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( Sqrt_2 );
 
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( Uint32Max, 4294967295.0 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( 1_Div_Uint32Max, (1.0 / 4294967295.0) );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( Uint64Max, 18446744073709551615.0 );
- 	GB_DECLARE_FLOAT_DOUBLE_CONST( 1_Div_Uint64Max, (1.0 / 18446744073709551615.0) );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( Uint32Max );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( 1_Div_Uint32Max );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( Uint64Max );
+ 	GB_DECLARE_FLOAT_DOUBLE_CONST( 1_Div_Uint64Max );
 
 #undef GB_DECLARE_FLOAT_DOUBLE_CONST
 
