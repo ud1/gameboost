e0b420ee866da04ee1361137abf70eab39d74b67
madness.cpp@gmail.com <madness.cpp@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-22 14:33:59 +0000

    http://code.google.com/p/gameboost/issues/detail?id=2

--- gb/base/Types.h
+++ gb/base/Types.h
@@ -1,20 +1,14 @@
 #pragma once
 
-#ifdef _WIN32
-	typedef signed		__int32 int32_t;
-	typedef unsigned	__int32 uint32_t;
-	typedef signed		__int64 int64_t;
-	typedef unsigned	__int64 uint64_t;
-	typedef signed		__int16 int16_t;
-	typedef unsigned	__int16 uint16_t;
-	typedef signed		__int8	int8_t;
-	typedef unsigned	__int8	uint8_t;
-#endif
-
-#ifdef MACOSX
-	#include <stdint.h>
-#endif
-
-#ifdef __linux
+#if defined(_MSC_VER) // MS Visual Studio dosen't have stdint.h
+	typedef __int8 int8_t;
+	typedef unsigned __int8 uint8_t;
+	typedef __int16 int16_t;
+	typedef unsigned __int16 uint16_t;
+	typedef __int32 int32_t;
+	typedef unsigned __int32 uint32_t;
+	typedef __int64 int64_t;
+	typedef unsigned __int64 uint64_t;
+#else // maybe elif defined(__GNUC__) || defined(__INTEL_COMPILER)
 	#include <stdint.h>
 #endif
