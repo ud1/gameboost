3ddbe638d5bcc3bd5e0b4fb5ded08cb659de469c
ksacvet777@gmail.com <ksacvet777@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-22 08:07:07 +0000

    Математические добавки. Некоторые улучшения.

--- trunk/gb/math/base.h
+++ trunk/gb/math/base.h
@@ -9,6 +9,22 @@
 *
 */
 
+/***********************************************************************
+
+
+ TODO:  
+   --- Поправить операторы для сравнения по эпсилону.
+
+
+   STORY:
+
+   ---  добавлены методы для векторов.
+   --- для структур векторов добавлены конструкторы
+   --  round теперь inline
+
+
+***********************************************************************/
+
 #pragma once
 
 #include <stdlib.h>
@@ -39,6 +55,14 @@ namespace gb
 				float floats [2];
 			};
 
+			    inline vec2_s() {};
+
+			    inline vec2_s(const vec2_s& v)      { x=v.x;  y=v.y;  };
+				inline vec2_s(const vec2_s* v)      { x=v->x;  y=v->y;    };
+
+			    inline vec2_s(float _x, float _y)   { x=_x;   y=_y;   };
+			    inline vec2_s(int   _x, int   _y)   { x=(float)_x;   y=(float)_y; };
+
 				inline bool  operator == (const vec2_s & v) const {	return (x == v.x && y == v.y  ); }
 				inline bool  operator != (const vec2_s & v) const {	return (x != v.x || y != v.y  ); }
 
@@ -47,6 +71,7 @@ namespace gb
 				inline bool  operator >  (const vec2_s & v) const {	return (x > v.x && y > v.y);     }
 				inline bool  operator >= (const vec2_s & v) const {	return (x >= v.x && y >= v.y );  }
 
+				inline vec2_s  operator + () const   { 	return *this; }
 				inline vec2_s  operator - () const   { vec2_s res; res.x = -x;	res.y = -y;	return res; }
 
 				inline vec2_s  operator + (const vec2_s &v) const { vec2_s res;	res = *this; res += v; return res; }
@@ -54,10 +79,15 @@ namespace gb
 				inline vec2_s  operator * (float f) const         { vec2_s res;	res = *this; res.x *= f;  res.y *= f; return res; }
 				inline vec2_s  operator / (float f) const         { vec2_s res;	res = *this; res.x /= f;  res.y /= f; return res; }
 
-				inline vec2_s &  operator += (const vec2_s &v) { x += v.x; y += v.y; return *this; }
-				inline vec2_s &  operator -= (const vec2_s &v) { x -= v.x; y -= v.y; return *this; }
-				inline vec2_s &  operator *= (float f)         { x *= f;   y *= f;	 return *this; }
-				inline vec2_s &  operator /= (float f)         { x /= f;   y /= f;   return *this; }
+				inline vec2_s &  operator += (const vec2_s &v)  { x += v.x; y += v.y;   return *this; }
+				inline vec2_s &  operator -= (const vec2_s &v)  { x -= v.x; y -= v.y;   return *this; }
+				inline vec2_s &  operator *= (float f)          { x *= f;   y *= f;	    return *this; }
+				inline vec2_s &  operator *= ( const vec2_s& v) { x *= v.x;	y *= v.y;	return *this;	}
+
+				inline vec2_s &  operator /= (float f)           { x /= f;    y /= f;   return *this; }
+				inline vec2_s &  operator /= ( const vec2_s& v ) { x /= v.x;  y /= v.y; return *this;	}
+
+
 
  			    inline operator  const float*() const  { return &x; };
 			    inline operator        float*()        { return &x; };
@@ -65,9 +95,25 @@ namespace gb
 				inline void setzero() {x=y=0.0f; };
 				inline bool empty() const { return ( (x==0.0f) && (y==0.0f) ); };
 
+				inline bool isZero(float epsilon) const
+				{
+					return( abs( x ) <= epsilon ) && ( abs( y ) <= epsilon ) ;
+				}
+
+				inline float     length () const  {	return (float)sqrt ( x*x + y*y );	}
+			    inline float     lengthSq() const {	 return (x*x + y*y );  }
+
+				inline float     dot(const vec2_s& v) const { return x*v.x + y*v.y; };
+							
 
 
-				inline void print() const { printf("%f %f", x, y); };
+
+				inline float     getMaxLength () const {  if( fabs (x) >= fabs (y) ) return x; else return y;   }
+
+
+  			    inline vec2_s&   invert() {x=-x; y=-y;  return *this; };
+
+				inline void print() const { printf(" %f %f ", x, y); };
 			};
 
 
@@ -83,6 +129,16 @@ namespace gb
 				float floats [3];
 			};
 
+			inline vec3_s() {};
+
+			inline vec3_s(const vec3_s& v)             { x=v.x;   y=v.y;   z=v.z;   };
+			inline vec3_s(const vec3_s* v)             { *this=*v; }; // x=v->x;  y=v->y; z=v->z;   };
+
+			inline vec3_s(const vec2_s& v, float _z)   { x=v.x;  y=v.y; z=_z;     };
+
+			inline vec3_s(float _x, float _y, float _z)   { x=_x;    y=_y;    z=_z;  };
+			inline vec3_s(int   _x, int   _y, int   _z)   { x=(float)_x;   y=(float)_y;  z=(float)_z; };
+
 					 
 			inline bool  operator == (const vec3_s &v) const { return (x == v.x && y == v.y && z == v.z ); }
 			inline bool  operator != (const vec3_s &v) const { return (x != v.x || y != v.y || z != v.z ); }
@@ -92,6 +148,7 @@ namespace gb
 			inline bool  operator >  (const vec3_s &v) const {	return (x >  v.x && y >  v.y && z >  v.z ); }
 			inline bool  operator >= (const vec3_s &v) const {	return (x >= v.x && y >= v.y && z >= v.z ); }
 
+			inline vec3_s  operator + () const   { 	return *this; }
 			inline vec3_s  operator - () const{	vec3_s res;	res.x = -x;	res.y = -y;	res.z = -z;	return res; }
 
 			inline vec3_s  operator + (const vec3_s &v) const {	vec3_s res;	res = *this; res += v;	return res; }
@@ -102,16 +159,45 @@ namespace gb
 			inline vec3_s &  operator += (const vec3_s &v)    {	x += v.x;	y += v.y;	z += v.z;	return *this; }
 			inline vec3_s &  operator -= (const vec3_s &v)    {	x -= v.x;	y -= v.y;	z -= v.z;	return *this; }
 			inline vec3_s &  operator *= (float f)            {	x *= f;	y *= f;	z *= f;	return *this; }
+			inline vec3_s &  operator *= (const vec3_s &v)    {	x *= v.x;	y *= v.y;	z *= v.z;	return *this; }
 			inline vec3_s &  operator /= (float f)            {	x /= f;	y /= f;	z /= f;	return *this; }
-
-			inline operator  const float*() const  { return &x; };
-			inline operator        float*()        { return &x; };
-
-		    inline void setzero() {x=y=z=0.0f; };
-			inline bool empty() const { return ( (x==0.0f) && (y==0.0f) && (z==0.0f) ); };
+			inline vec3_s &  operator /= (const vec3_s &v)    {	x /= v.x;	y /= v.y;	z /= v.z;	return *this; }
 
 
- 			inline void print() const { printf("%f %f %f", x, y, z); };
+			inline operator  const float*() const  { return &x; }
+			inline operator        float*()        { return &x; }
+
+		    inline void setzero() {x=y=z=0.0f; }
+			inline bool empty() const { return ( (x==0.0f) && (y==0.0f) && (z==0.0f) ); }
+
+			inline bool isZero(float epsilon) const
+			{
+				return( abs( x ) <= epsilon ) && ( abs( y ) <= epsilon ) && ( abs( z ) <= epsilon )  ;
+			}
+
+			inline float     length () const {	return (float)sqrt ( x*x + y*y + z*z );	}
+			inline float     lengthSq() const {	 return (x*x + y*y + z*z);  }
+
+			inline float     dot(const vec3_s& v) const { return x*v.x + y*v.y + z*v.z; };
+
+            #pragma message ("KS777:  NEED CHECK !!!"  __FILE__)
+			inline vec3_s    cross (const vec3_s &v) const
+			{
+				vec3_s r;
+				   r.x = y * v.z - z * v.y;
+				   r.y = z * v.x - x * v.z;
+				   r.z = x * v.y - y * v.x;
+				return r;
+			}
+
+			inline vec3_s&   normalize ()	  { register float fl=length(); x/=fl; y/=fl; z/=fl; return *this; }
+			inline vec3_s    getNormalized() const { vec3_s r=*this; r.normalize(); return r; };
+ 
+			inline float     getMaxLength () const {   return scalar::max3 ( fabs (x), fabs (y), fabs (z) );   }
+
+			inline vec3_s&   invert() {x=-x; y=-y; z=-z; return *this; };
+
+ 			inline void print() const { printf(" %f %f %f ", x, y, z); };
 
 		}; // end vec3_s
 		
@@ -129,6 +215,16 @@ namespace gb
 				float floats [4];
 			};
 
+			inline vec4_s() {};
+
+			inline vec4_s(const vec4_s& v)      { x=v.x;  y=v.y; z=v.z; w=v.w;  };
+			inline vec4_s(const vec4_s* v)      { x=v->x;  y=v->y; z=v->z; w=v->w;  };
+
+			inline vec4_s(const vec3_s& v, float _w)  { x=v.x;  y=v.y; z=v.z; w=_w;  };
+
+			inline vec4_s(float _x, float _y, float _z, float _w)   { x=_x;   y=_y; z=_z; w=_w;  };
+			inline vec4_s(int   _x, int   _y, int   _z, int   _w)   { x=(float)_x;   y=(float)_y;  z=(float)_z; w=(float)_w;};
+
 					 
 			inline bool  operator == (const vec4_s &v) const {	return (x == v.x && y == v.y && z == v.z && w == v.w); }
 			inline bool  operator != (const vec4_s &v) const {	return (x != v.x || y != v.y || z != v.z || w != v.w); }
@@ -138,7 +234,8 @@ namespace gb
 			inline bool  operator >  (const vec4_s &v) const {	return (x > v.x && y > v.y && z > v.z && w > v.w); }
 			inline bool  operator >= (const vec4_s &v) const {	return (x >= v.x && y >= v.y && z >= v.z && w >= v.w); }
 
-			inline vec4_s  operator - () const{	vec4_s res;	res.x = -x;	res.y = -y;	res.z = -z;	res.w = -w;	return res; }
+			inline vec4_s  operator + () const   { 	return *this; }
+			inline vec4_s  operator - () const { vec4_s res;	res.x = -x;	res.y = -y;	res.z = -z;	res.w = -w;	return res; }
 
 			inline vec4_s  operator + (const vec4_s &v) const {	vec4_s res;	res = *this;	res += v;	return res; }
 			inline vec4_s  operator - (const vec4_s &v) const {	vec4_s res;	res = *this;	res -= v;	return res; }
@@ -148,6 +245,7 @@ namespace gb
 			inline vec4_s &  operator += (const vec4_s &v) {	x += v.x;	y += v.y;	z += v.z;	w += v.w;	return *this; }
 			inline vec4_s &  operator -= (const vec4_s &v) {	x -= v.x;	y -= v.y;	z -= v.z;	w -= v.w;	return *this; }
 			inline vec4_s &  operator *= (float f)         {	x *= f;	y *= f;	z *= f;	w *= f;	return *this; }
+
 			inline vec4_s &  operator /= (float f)         {	x /= f;	y /= f;	z /= f;	w /= f;	return *this; }
 
 			inline operator  const float*() const  { return &x; };
@@ -156,9 +254,32 @@ namespace gb
 		    inline void setzero() {x=y=z=w=0.0f; };
 			inline bool empty() const { return ( (x==0.0f) && (y==0.0f) && (z==0.0f) && (w==0.0f) ); };
 
+			inline bool isZero(float epsilon) const
+			{
+				return( abs( x ) <= epsilon ) && ( abs( y ) <= epsilon ) && ( abs( z ) <= epsilon ) && ( abs( y ) <= epsilon );
+			}
+
+			inline float length () const {	return (float)sqrt( x*x + y*y + z*z + w*w );	}
+			inline float lengthSq() const {	 return (x*x + y*y + z*z + w*w);  }
+
+			inline float   dot (const vec4_s& v) const { return x*v.x + y*v.y + z*v.z + w*v.w; }
+
+            #pragma message ("KS777:  NEED CHECK !!!"  __FILE__)
+			inline vec4_s  cross ( const vec4_s & v) const
+			{
+				  vec4_s r;
+				r.x = ( z * v.w - w * v.z) * y + ( w * v.y - y * v.w) * z + ( y * v.z - z * v.y) * w;
+				r.y = ( w * v.z - z * v.w) * x + ( x * v.w - w * v.x) * z + ( z * v.x - x * v.z) * w;
+				r.z = ( y * v.w - w * v.y) * x + ( w * v.x - x * v.w) * y + ( x * v.y - y * v.x) * w;
+				r.w = ( z * v.y - y * v.z) * x + ( x * v.z - z * v.x) * y + ( y * v.x - x * v.y) * z;
+				  return r;
+			}
 
 
-			inline void print() const { printf("%f %f %f %f", x, y, z, w); };
+			inline vec4_s&   invert() {x=-x; y=-y; z=-z; w=-w; return *this; };
+
+
+			inline void print() const { printf(" %f %f %f %f ", x, y, z, w); };
 
 		}; // end vec4_s
 		
@@ -181,47 +302,169 @@ namespace gb
 			inline operator  const float*() const  { return &_11; };
 			inline operator        float*()        { return &_11; };
 
-			
-			mat22_s&  operator =  ( const mat22_s& m);
-			/** \brief   В главную диагональ установить f остальное занулить.*/
-			mat22_s&  operator =  ( float f);
-			/** \breief  Покомпонентное сложение   (this = this + m) */
-			mat22_s&  operator += ( const mat22_s& m);
-			/** \breief  Покомпонентное вычитанние (this = this - m) */
-			mat22_s&  operator -= ( const mat22_s& m);
 
-			mat22_s&  operator *= ( const mat22_s& m);
+			inline mat22_s&  operator =  ( const mat22_s& m)
+			{
+				floats [0][0] = m.floats [0][0]; 
+				floats [0][1] = m.floats [0][1]; 
+				floats [1][0] = m.floats [1][0];
+				floats [1][1] = m.floats [1][1];
+				return *this;
+			};
+
+			/** \brief   В главную диагональ установить f   остальное занулить.*/
+			inline mat22_s&  operator =  ( float f) { 	floats [0][1] = floats [1][0] = 0.0; floats [0][0] = floats [1][1] = f; };
+
+			/** \breief  Покомпонентное сложение   (this = this + m) */
+			inline mat22_s&  operator += ( const mat22_s& m)
+			{
+				floats [0][0] += m.floats [0][0];
+				floats [0][1] += m.floats [0][1];
+				floats [1][0] += m.floats [1][0];
+				floats [1][1] += m.floats [1][1];
+				return *this;
+			};
+
+			/** \breief  Покомпонентное вычитанние (this = this - m) */
+			inline mat22_s&  operator -= ( const mat22_s& m)
+			{
+				floats [0][0] -= m.floats [0][0];
+				floats [0][1] -= m.floats [0][1];
+				floats [1][0] -= m.floats [1][0];
+				floats [1][1] -= m.floats [1][1];
+				return *this;
+			};
+
+			inline mat22_s&  operator *= ( const mat22_s& m)
+			{
+				mat22_s c  = *this ;
+				floats [0][0] = c.floats [0][0]*m.floats [0][0] + c.floats [0][1]*m.floats [1][0];
+				floats [0][1] = c.floats [0][0]*m.floats [0][1] + c.floats [0][1]*m.floats [1][1];
+				floats [1][0] = c.floats [1][0]*m.floats [0][0] + c.floats [1][1]*m.floats [1][0];
+				floats [1][1] = c.floats [1][0]*m.floats [0][1] + c.floats [1][1]*m.floats [1][1];
+				return *this;
+			};
 
 			/** \breief  Покомпонентное умножение (this = this * m) */            
-			mat22_s&  operator *= ( float f);
+			inline mat22_s&  operator *= ( float f)
+			{
+				floats [0][0] *= f;
+				floats [0][1] *= f;
+				floats [1][0] *= f;
+				floats [1][1] *= f;
+				return *this;
+			};
+
 			/** \breief  Покомпонентное деление (this = this / m) */
-			mat22_s&  operator /= ( float f);
+			inline mat22_s&  operator /= ( float f)
+			{
+				floats [0][0] /= f;
+				floats [0][1] /= f;
+				floats [1][0] /= f;
+				floats [1][1] /= f;
+				return *this;
+			};
 
-			mat22_s operator + ( const mat22_s& m) const;
-			mat22_s operator - ( const mat22_s& m) const;
+			inline mat22_s operator + ( const mat22_s& m) const
+			{
+				mat22_s res;
+				  res.floats[0][0] = floats [0][0] + m.floats [0][0];
+				  res.floats[0][1] = floats [0][1] + m.floats [0][1];
+				  res.floats[1][0] = floats [1][0] + m.floats [1][0];
+				  res.floats[1][1] = floats [1][1] + m.floats [1][1];
+				     return res;
+			};
 
-			mat22_s operator * ( const mat22_s& m) const;
-			mat22_s operator * ( float f ) const;
-			vec2_s  operator * ( const vec2_s& v ) const;
+			inline mat22_s operator - ( const mat22_s& m) const
+			{
+				  mat22_s res;
+				res.floats [0][0] = floats [0][0] - m.floats [0][0];
+				res.floats [0][1] = floats [0][1] - m.floats [0][1];
+				res.floats [1][0] = floats [1][0] - m.floats [1][0];
+				res.floats [1][1] = floats [1][1] - m.floats [1][1];
+				return res;
+			};
+
+			inline mat22_s operator * ( const mat22_s& m) const
+			{
+				  mat22_s res;
+				res.floats[0][0] = floats[0][0]*m.floats[0][0]+floats[0][1]*m.floats[1][0];
+				res.floats[0][1] = floats[0][0]*m.floats[0][1]+floats[0][1]*m.floats[1][1];
+				res.floats[1][0] = floats[1][0]*m.floats[0][0]+floats[1][1]*m.floats[1][0];
+				res.floats[1][1] = floats[1][0]*m.floats[0][1]+floats[1][1]*m.floats[1][1];
+				return res;
+			};
+
+			inline mat22_s operator * ( float f ) const
+			{
+				  mat22_s res;
+				res.floats[0][0] = floats[0][0] * f;
+				res.floats[0][1] = floats[0][1] * f;
+				res.floats[1][0] = floats[1][0] * f;
+				res.floats[1][1] = floats[1][1] * f;
+				return  res;
+			};
+
+			inline vec2_s  operator * ( const vec2_s& v ) const
+			{
+				vec2_s res; 
+				res.x =  floats [0][0]*v.x + floats [0][1]*v.y ;
+				res.y =  floats [1][0]*v.x + floats [1][1]*v.y ;
+				return res;
+			};
 
 
-			inline void setzero()     { _11=_12=_21=_22=0.0f; };
-			inline void setIdentity() {	_11=1.0f; _12=0.0f;	_21=0.0f; _22=1.0f;	};
-			inline void transpone()   {  register float f=_12; _12=_21; _21=f; };
+			inline mat22_s&  setzero()     { _11=_12=_21=_22=0.0f; return *this; };
+			inline mat22_s&  setIdentity() {	_11=1.0f; _12=0.0f;	_21=0.0f; _22=1.0f;	return *this; };
+			inline mat22_s&  transpone()   {  register float f=_12; _12=_21; _21=f;  return *this; };
 
-			inline float determinant () { return floats [0][0] * floats [1][1] - floats [0][1] * floats [1][0];	};
+			inline float determinant () const { return floats [0][0] * floats [1][1] - floats [0][1] * floats [1][0];	};
 
-            mat22_s&  invert ();
+			inline mat22_s&  invert ()
+			{
+			   float det =   determinant();  
+			   mat22_s m;
+				 m.floats [0][0] =  floats [1][1] / det;
+				 m.floats [0][1] = -floats [0][1] / det;
+				 m.floats [1][0] = -floats [1][0] / det;
+				 m.floats [1][1] =  floats [0][0] / det;
+				     *this = m;
+				     return *this;
+			};
 
-			mat22_s&         setRotation ( float angle );
+			inline mat22_s   getInverted () const { mat22_s r(*this); r.invert(); return r;  };
+
+			inline mat22_s&  setScaling ( const vec2_s& v )
+			{
+				   floats [0][0] = v.x;
+				   floats [1][1] = v.y;
+				   floats [0][1] = floats [1][0] = 0.0;
+				   return *this;
+			};
+
+			inline mat22_s&   setScaling (float x, float y)
+			{
+				floats [0][0] = x;
+				floats [1][1] = y;
+				    floats [0][1] = floats [1][0] = 0.0;
+				    return *this;
+			};
+
+			mat22_s&  setRotation ( float angle )
+			{
+				float    cosine, sine;
+				scalar::sincos(angle , sine , cosine );
+				  floats [0][0] = cosine;
+				  floats [0][1] = sine;
+				  floats [1][0] = -sine;
+				  floats [1][1] = cosine;
+				     return *this;
+			};
+
+
 			inline mat22_s&  setMirrorX () { setIdentity();  floats [0][0] = -1.0; return *this; };
 			inline mat22_s&  setMirrorY () { setIdentity();  floats [1][1] = -1.0; return *this; };
-			mat22_s&         setScaling ( const vec2_s& v );
-			mat22_s&         setScaling (float x, float y);
-
-
-
-
+ 
 
 		}; 
 
@@ -293,13 +536,7 @@ namespace gb
 			mat33_s&  setMirrorX();
 			mat33_s&  setMirrorY();
 			mat33_s&  setMirrorZ();
-
-
-
-
-
-
-
+ 
 		
 		};
  
@@ -324,16 +561,26 @@ namespace gb
 			inline operator  const float*() const  { return &_11; };
 			inline operator        float*()        { return &_11; };
 
+			//
 
-			inline void setzero() { memset(&_11, 0, sizeof(mat44_s)  ); };
-			inline void setIdentity() {
+
+
+
+
+
+
+
+			inline void       setzero() { memset(&_11, 0, sizeof(mat44_s)  ); };
+			inline mat44_s&   setIdentity() {
 				_11=1.0f; _12=0.0f; _13=0.0f; _14=0.0f;
 				_21=0.0f; _22=1.0f; _23=0.0f; _24=0.0f; 
 				_31=0.0f; _32=0.0f; _33=1.0f; _34=0.0f;  
 				_41=0.0f; _42=0.0f; _43=0.0f; _44=1.0f; 
+				 return *this;
 	        };
 
-			inline void transpone() {  
+			inline mat44_s& transpone() 
+			{  
 				   register float f;
 				f = _12;  _12 = _21;  _21 = f;
 				f = _13;  _13 = _31;  _31 = f;
@@ -341,11 +588,14 @@ namespace gb
 				f = _23;  _23 = _32;  _32 = f;
 				f = _24;  _24 = _42;  _42 = f;
 				f = _34;  _34 = _43;  _43 = f; 
+				  return *this;
 			};
 
 
 			#ifdef GB_OPENGL
+			   /** \brief Старый способ загрузки матрицы в OpenGL */
                inline void glMul()  { glMultMatrixf( (GLfloat*)&_11  ); };
+			   /** \brief Старый способ перемножения матрицы  с текущей в OpenGL */
                inline void glLoad() { glLoadMatrixf( (GLfloat*)&_11  ); };
             #endif
 
--- trunk/test/math/test0.cpp
+++ trunk/test/math/test0.cpp
@@ -1,7 +1,9 @@
 // test math
 //
 
+//#pragma message("KS777: NEED COMMENTED"  __FILE__ )
 //#include "stdafx.h"
+
 #include <windows.h>
 
 //#include "z:\\!!GB_TOADD\\math\\base.h"
@@ -17,19 +19,62 @@ namespace gm = gb::math;
 namespace gmb = gb::math::base;
 namespace gms = gb::math::scalar;
 
- 
+
+//inline  int new_round(float f)
+//{
+//    // float -> integer cast uses truncation (i.e. round to zero), 
+//	//according to C standard; offset the value to convert 
+//	//round-to-zero to round-to-nearest
+//    return (int)(f + (f > 0 ? 0.5f : -0.5f));
+//}
+// 
+
+//=========================================================================
+void test_round() {
+	printf("test round\n");
+
+ printf("--------gb::math::scalar::round----------\n");
+ for(int c=-10; c<=10; c++)
+ {
+  float  finp = ((float)c) *0.2f   ;
+  int nr = gb::math::scalar::round(finp);
+  printf("Input: %f    out: %i  \n", finp , nr);
+ }
+
+  // new_round
+ printf("--------gb::math::scalar::roundAsm----------\n");
+
+ for(int c=-10; c<=10; c++)
+ {
+  float  finp = ((float)c) *0.2f   ;
+  int nr =  gb::math::scalar::roundAsm(finp);
+  printf("Input: %f    out: %i  \n", finp , nr);
+ }
+
+
+
+};
 
  //=======================================================================
 int main( int argc,  char* argv[] )
 {
-	int val = gb::math::scalar::round(5353.6457f); 
-	val = gb::math::scalar::roundAsm( 5353.6457f );
+   test_round();
+
+
+	float  val  = 5.5345f;
+	float vsin, vcos;
+	gb::math::scalar::sincos(val, vsin , vcos);
+
+
+	// round
+	int nval = gb::math::scalar::round(5353.6457f); 
+	nval = gb::math::scalar::roundAsm( 5353.6457f );
 
 
 
 
 
-
+  system("pause");
 	return 0;
 }
 
