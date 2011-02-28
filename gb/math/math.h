7f17eafa4976d3fa9be3d18596c17f7926637cea
ksacvet777@gmail.com <ksacvet777@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-02-28 08:15:18 +0000

    small chngs ...

---
+++ gb/math/math.h
@@ -0,0 +1,18 @@
+/** \file math.h
+ \brief Для включения всех math-либ.
+*
+*
+*
+*/
+
+
+
+#include <gb/math/scalar.h>
+#include <gb/math/base.h>
+#include <gb/math/geom2d.h>
+#include <gb/math/geom3d.h>
+
+
+
+
+// end file
