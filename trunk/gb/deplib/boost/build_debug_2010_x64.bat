067eeda2254f52edb80d5766b50b9614a3398220
enegine@gmail.com <enegine@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-05-21 08:25:43 +0000

    Удален rand

--- trunk/gb/deplib/boost/build_debug_2010_x64.bat
+++
@@ -1,4 +0,0 @@
-@rem bjam runtime-link=static link=static threading=multi variant=debug
-bjam runtime-link=shared link=static threading=multi variant=debug toolset=msvc-10.0 address-model=64 -j4
-
-
