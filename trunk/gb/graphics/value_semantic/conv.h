0a36d9018b287bad9eecdd952cf16a9fabd93d23
ksacvet777@gmail.com <ksacvet777@gmail.com@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-05-23 23:35:14 +0000

    поврежденые файлы

--- trunk/gb/graphics/value_semantic/conv.h
+++ trunk/gb/graphics/value_semantic/conv.h
@@ -1,7 +1,1 @@
-var begunAds = {
-"banners" : {
-    "autocontext" : [
-        {
-            "title" : "<b>&#x418;&#x43d;&#x442;&#x435;&#x440;&#x43d;&#x435;&#x442;</b>-<b>&#x43c;&#x430;&#x433;&#x430;&#x437;&#x438;&#x43d;</b> <b>&#x43e;&#x434;&#x435;&#x436;&#x434;&#x44b;</b>",
-            "descr" : "&#x418;&#x437;&#x432;&#x435;&#x441;&#x442;&#x43d;&#x44b;&#x435; &#x431;&#x440;&#x435;&#x43d;&#x434;&#x44b; &#x441;&#x43e; &#x441;&#x43a;&#x438;&#x434;&#x43a;&#x43e;&#x439; &#x434;&#x43e; 70%.&#10;&#x410;&#x43a;&#x446;&#x438;&#x438; &#x43f;&#x43e; 3 &#x434;&#x43d;&#x44f;. &#x423;&#x441;&#x43f;&#x435;&#x439; &#x441;&#x435;&#x433;&#x43e;&#x434;&#x43d;&#x44f;!",
-            "url" : "http://click02.begun.ru/click.jsp?url=HvHPg8jAwcBKkWFnL*0h4kyxsa0KMErFEXILupaFuEspsd81c28fvl05reTvdX0cn-OiSm
+#error файл был повреждён.  Переделать
