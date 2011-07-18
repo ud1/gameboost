35438110cca7f562dced6bebd94cb45fbf2b25e9
ksacvet777 <ksacvet777@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-07-18 06:06:11 +0000

    prepare templated

---
+++ trunk/gb/fmath/matrix.hpp
@@ -0,0 +1,39 @@
+﻿
+#ifndef __MATRIX_HPP__
+#define __MATRIX_HPP__
+
+
+#pragma once
+
+#ifndef __GB_FMATH_H__
+    #error НЕ ВКЛЮЧАЙТЕ ЭТОТ ФАЙЛ. ВКЛЮЧАЙТЕ:   #include <gb/fmath/math.h>  
+#endif
+
+#include <boost/array.hpp>
+#include <iostream>
+#include <string>
+
+namespace gb
+{
+
+	namespace fmath
+	{
+
+	template <typename T, size_t NumColumns, size_t NumRows>
+	struct matrix : public boost::array< T , NumColumns * NumRows > {
+	  /// ....
+ 
+
+
+	};
+
+
+
+
+
+
+	}
+
+}
+
+#endif // __MATRIX_HPP__
---
+++ trunk/gb/fmath/matrix4x4.hpp
@@ -0,0 +1,40 @@
+﻿// matrix4x4.hpp
+
+#ifndef __MATRIX_4X4_HPP__
+#define __MATRIX_4X4_HPP__
+
+
+#pragma once
+
+#ifndef __GB_FMATH_H__
+    #error НЕ ВКЛЮЧАЙТЕ ЭТОТ ФАЙЛ. ВКЛЮЧАЙТЕ:   #include <gb/fmath/math.h>  
+#endif
+
+#include "matrix_quad.hpp"
+
+
+#include <boost/array.hpp>
+
+namespace gb
+{
+
+	namespace fmath
+	{
+	
+ 
+
+	template <typename T>
+	struct matrix4x4 : public matrix_quad<T,4> {
+	 /// ....
+	 
+	};
+
+ 
+
+
+	
+	
+	}
+}
+
+#endif // __MATRIX_4X4_HPP__
