5aaa5d1687e1c53e0e48999666a79ce2f26141a8
ksacvet777 <ksacvet777@74cdb5a8-5d8c-df42-f2c8-9e89128277d9>
2011-08-04 09:47:36 +0000

    vg  расформирование и перенос .

--- gb/graphics/opengl/gl_render_helper.cpp
+++ gb/graphics/opengl/gl_render_helper.cpp
@@ -1,111 +1,111 @@
 ﻿
 
-#include "gl_render_helper.h"
+// #include "gl_render_helper.h"
 
-namespace gb
-{
- namespace graphics
- {
-  namespace opengl
-  {
+// namespace gb
+// {
+ // namespace graphics
+ // {
+  // namespace opengl
+  // {
   
   
  
-				void gl_render_helper::clear_deph()
-				{
-					glClear(GL_DEPTH_BUFFER_BIT); 
-					GLenum err = glGetError();
-					if(err != GL_NO_ERROR)
-					{
-						assert(false);
-						throw std::runtime_error("operation error");
-					}
-				}
+				// void gl_render_helper::clear_deph()
+				// {
+					// glClear(GL_DEPTH_BUFFER_BIT); 
+					// GLenum err = glGetError();
+					// if(err != GL_NO_ERROR)
+					// {
+						// assert(false);
+						// throw std::runtime_error("operation error");
+					// }
+				// }
 
-				void gl_render_helper::clear_surface(const color::color3<float>* color)
-				{
-					float _r = 0.0f;
-					float _g = 0.0f;
-					float _b = 0.0f;
+				// void gl_render_helper::clear_surface(const color::color3<float>* color)
+				// {
+					// float _r = 0.0f;
+					// float _g = 0.0f;
+					// float _b = 0.0f;
 
-					if(color)
-					{
-						_r = color->r;
-						_g = color->g;
-						_b = color->b;
-					}
+					// if(color)
+					// {
+						// _r = color->r;
+						// _g = color->g;
+						// _b = color->b;
+					// }
 
-					glClearColor( _r, _g, _b, 1.0f );
-					glClear(GL_COLOR_BUFFER_BIT);
-					GLenum err = glGetError();
-					if(err != GL_NO_ERROR)
-					{
-						assert(false);
-						throw std::runtime_error("operation error");
-					}
-				}
+					// glClearColor( _r, _g, _b, 1.0f );
+					// glClear(GL_COLOR_BUFFER_BIT);
+					// GLenum err = glGetError();
+					// if(err != GL_NO_ERROR)
+					// {
+						// assert(false);
+						// throw std::runtime_error("operation error");
+					// }
+				// }
  
 
-				void gl_render_helper::set_matrix(const math::mat4* world, const math::mat4* view, const math::mat4* proj)
-				{
+				// void gl_render_helper::set_matrix(const math::mat4* world, const math::mat4* view, const math::mat4* proj)
+				// {
 
-				#pragma message( "  INSERT CHECK  CODE ! ....  "  )
-					assert(false);
+				// #pragma message( "  INSERT CHECK  CODE ! ....  "  )
+					// assert(false);
 
-					// deprecated (holder)
+					//deprecated (holder)
 
-					// safe matrix mode
-					int mmode = 0;
-					glGetIntegerv(GL_MATRIX_MODE , &mmode);
+					//safe matrix mode
+					// int mmode = 0;
+					// glGetIntegerv(GL_MATRIX_MODE , &mmode);
 
-					// set new transform
+					//set new transform
 
-					if(world)
-					{
-						glMatrixMode(GL_MODELVIEW  );
-						math::mat4 modelview;
-						if(view)
-						{
-							modelview = *world   *   *view;
-						}
-						else
-						{
-							math::mat4 mview = get_matrix_view();
-							modelview = *world   *  mview;
-						}
+					// if(world)
+					// {
+						// glMatrixMode(GL_MODELVIEW  );
+						// math::mat4 modelview;
+						// if(view)
+						// {
+							// modelview = *world   *   *view;
+						// }
+						// else
+						// {
+							// math::mat4 mview = get_matrix_view();
+							// modelview = *world   *  mview;
+						// }
 
-						glLoadMatrixf(  (GLfloat*)&modelview );
-					}
+						// glLoadMatrixf(  (GLfloat*)&modelview );
+					// }
 
-					if(view)
-					{
-						glMatrixMode(GL_MODELVIEW  );
-						math::mat4 modelview;
-						if(world)
-						{	
-							modelview = *world   *   *view;
-						}
-						else
-						{
-							math::mat4 mworld = get_matrix_world();
-							modelview = mworld   *   *view;
-						}
+					// if(view)
+					// {
+						// glMatrixMode(GL_MODELVIEW  );
+						// math::mat4 modelview;
+						// if(world)
+						// {	
+							// modelview = *world   *   *view;
+						// }
+						// else
+						// {
+							// math::mat4 mworld = get_matrix_world();
+							// modelview = mworld   *   *view;
+						// }
 
-						glLoadMatrixf(  (GLfloat*)&modelview );
+						// glLoadMatrixf(  (GLfloat*)&modelview );
 
-					}
+					// }
 
-					if(proj)
-					{
-						glMatrixMode(GL_PROJECTION );
-						glLoadMatrixf( (GLfloat*)proj );
-					}
+					// if(proj)
+					// {
+						// glMatrixMode(GL_PROJECTION );
+						// glLoadMatrixf( (GLfloat*)proj );
+					// }
 
-					// restore matrix mode 
-					glMatrixMode(mmode);
+					//restore matrix mode 
+					// glMatrixMode(mmode);
 
 
-				}
+				// }
 
  
   
@@ -115,8 +115,8 @@ namespace gb
   
   
   
-  }
- }
-}
+  // }
+ // }
+// }
 
 
